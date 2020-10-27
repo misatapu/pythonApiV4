ca9669bf6737e6c92b649dc3716deb6a58ff0b8c
misatapu <70611841+misatapu@users.noreply.github.com>
2020-10-27 08:21:32 -0500

    Update client.py

--- service/client.py
+++ service/client.py
@@ -8,10 +8,12 @@ class Client():
         self.doc_id = doc_id
         self.preexistence = []
 
+    """ Adds a preexistence to a client """
     def add_preexistence(self, n_preexistence):
         self.preexistence.append(n_preexistence)
         return len(self.preexistence) - 1
 
+    """ Adds a preexistence to a client """
     def get_preexistence(self, p_index):
         if p_index >= len(self.preexistence):
             return 'There is no such preexistence'
